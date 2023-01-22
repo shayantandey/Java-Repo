51388f94c3ceba21ef126aaf0f06fdc4bcca07ed
Shayantan Dey <shayantan.dey@gmail.com>
2023-01-23 01:15:25 +0530

    Number operations

---
+++ NumberOperations/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to a new day!");
+        int numberOne = 5;
+        int numberTwo = 10;
+        System.out.println("Addition : "+ (numberOne+numberTwo));
+        System.out.println("Multiplication : "+ (numberOne*numberTwo));
+    }
+}
